a36ee23a4dd8b41e72ca11453735e21dd4e9ed5d
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-06-08 04:10:53 +0300

    add new theme & login method

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:digital_wallet/view/screens/auth_screen.dart';
+import 'package:digital_wallet/view/screens/homepage.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'controller/auth_provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,12 +14,27 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'My Wallet',
-      theme: ThemeData(
-        primaryColor: const Color.fromARGB(1, 0, 184, 148),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => AuthProvider(),
+        ),
+      ],
+      child: Consumer<AuthProvider>(
+        builder: (ctx, auth, ch) => MaterialApp(
+          title: 'My Wallet',
+          theme: ThemeData(
+            fontFamily: 'Tajawal',
+            primaryColor: const Color.fromARGB(1, 0, 184, 148),
+            colorScheme: const ColorScheme.light(
+              primary: Color.fromARGB(255, 10, 75, 149),
+              secondary: Color.fromARGB(255, 0, 212, 112),
+              background: Color.fromARGB(255, 243, 243, 243),
+            )
+          ),
+          home: auth.isAuth ? HomePage() : const AuthScreen(),
+        ),
       ),
-      home: const AuthScreen(),
     );
   }
 }
