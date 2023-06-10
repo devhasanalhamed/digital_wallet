b48506223bf7c97704aea3fb66323cbbfe69a25d
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-06-10 06:06:58 +0300

    Create app_structure_provider.dart
    in this page we can control the number and the type of the actions in the user interface

---
+++ lib/controller/app_structure_provider.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class AppStructure with ChangeNotifier {
+  final homepageCategories = [
+    {'title': 'نقل أموال', 'icon': FontAwesomeIcons.moneyBills},
+    {'title': 'إضافة مال', 'icon': FontAwesomeIcons.moneyBill},
+    {'title': 'حوالة دولية', 'icon': FontAwesomeIcons.earthAsia},
+    {'title': 'حوالة محلية', 'icon': FontAwesomeIcons.buildingColumns},
+    {'title': 'إصدار فاتورة', 'icon': FontAwesomeIcons.sheetPlastic},
+    {'title': 'دفع الفواتير', 'icon': FontAwesomeIcons.mobile},
+    {'title': '7', 'icon': FontAwesomeIcons.earthAsia},
+    {'title': '8', 'icon': FontAwesomeIcons.earthAsia},
+    {'title': '9', 'icon': FontAwesomeIcons.earthAsia},
+    {'title': '10', 'icon': FontAwesomeIcons.earthAsia},
+    {'title': '11', 'icon': FontAwesomeIcons.earthAsia},
+    {'title': '12', 'icon': FontAwesomeIcons.earthAsia},
+    {'title': '13', 'icon': FontAwesomeIcons.earthAsia},
+    {'title': '14', 'icon': FontAwesomeIcons.earthAsia},
+    {'title': '15', 'icon': FontAwesomeIcons.earthAsia},
+    {'title': '16', 'icon': FontAwesomeIcons.earthAsia},
+    {'title': '17', 'icon': FontAwesomeIcons.earthAsia},
+  ];
+}
