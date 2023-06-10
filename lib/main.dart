cf5a350f598dd4011bf536d58af9229d84847dab
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-06-10 06:07:28 +0300

    Update main.dart
    App Architecture provider added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:digital_wallet/controller/app_structure_provider.dart';
 import 'package:digital_wallet/view/screens/auth_screen.dart';
 import 'package:digital_wallet/view/screens/homepage.dart';
 import 'package:flutter/material.dart';
@@ -17,6 +18,9 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(
+          create: (ctx) => AppStructure(),
+        ),
+        ChangeNotifierProvider(
           create: (ctx) => AuthProvider(),
         ),
       ],
@@ -24,15 +28,15 @@ class MyApp extends StatelessWidget {
         builder: (ctx, auth, ch) => MaterialApp(
           title: 'My Wallet',
           theme: ThemeData(
-            fontFamily: 'Tajawal',
-            primaryColor: const Color.fromARGB(1, 0, 184, 148),
-            colorScheme: const ColorScheme.light(
-              primary: Color.fromARGB(255, 10, 75, 149),
-              secondary: Color.fromARGB(255, 0, 212, 112),
-              background: Color.fromARGB(255, 243, 243, 243),
-            )
-          ),
-          home: auth.isAuth ? HomePage() : const AuthScreen(),
+              fontFamily: 'Tajawal',
+              primaryColor: const Color.fromARGB(1, 0, 184, 148),
+              colorScheme: const ColorScheme.light(
+                primary: Color.fromARGB(255, 10, 75, 149),
+                secondary: Color.fromARGB(255, 0, 212, 112),
+                background: Color.fromARGB(255, 243, 243, 243),
+              )),
+          // home: auth.isAuth ? HomePage() : const AuthScreen(),
+          home: const HomePage(),
         ),
       ),
     );
