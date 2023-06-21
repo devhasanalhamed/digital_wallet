af99d10ecf0e35dc4c23aad7adc6f27cbe7535f0
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-06-21 21:34:47 +0300

    new model

--- lib/model/user_model.dart
+++ lib/model/user_model.dart
@@ -1,9 +1,21 @@
 class UserModel {
+  String firstName;
+  String fatherName;
+  String grandFatherName;
+  String lastName;
   String phoneNumber;
   String password;
+  DateTime? dateOfBirth;
+  String jobTitle;
 
   UserModel({
+    required this.firstName,
+    required this.fatherName,
+    required this.grandFatherName,
+    required this.lastName,
     required this.phoneNumber,
     required this.password,
+    required this.dateOfBirth,
+    required this.jobTitle,
   });
 }
