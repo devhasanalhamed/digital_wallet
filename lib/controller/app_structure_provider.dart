b8d02e8ab645563f6fbeb01d23abd53d557f9590
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-06-21 21:33:41 +0300

    Update app_structure_provider.dart
    remove the app structure

--- lib/controller/app_structure_provider.dart
+++ lib/controller/app_structure_provider.dart
@@ -3,7 +3,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class AppStructure with ChangeNotifier {
   final homepageCategories = [
-    {'title': 'نقل أموال', 'icon': FontAwesomeIcons.moneyBills},
+    {'title': 'إرسال أموال', 'icon': FontAwesomeIcons.moneyBills},
     {'title': 'إضافة مال', 'icon': FontAwesomeIcons.moneyBill},
     {'title': 'حوالة دولية', 'icon': FontAwesomeIcons.earthAsia},
     {'title': 'حوالة محلية', 'icon': FontAwesomeIcons.buildingColumns},
@@ -12,13 +12,5 @@ class AppStructure with ChangeNotifier {
     {'title': '7', 'icon': FontAwesomeIcons.earthAsia},
     {'title': '8', 'icon': FontAwesomeIcons.earthAsia},
     {'title': '9', 'icon': FontAwesomeIcons.earthAsia},
-    {'title': '10', 'icon': FontAwesomeIcons.earthAsia},
-    {'title': '11', 'icon': FontAwesomeIcons.earthAsia},
-    {'title': '12', 'icon': FontAwesomeIcons.earthAsia},
-    {'title': '13', 'icon': FontAwesomeIcons.earthAsia},
-    {'title': '14', 'icon': FontAwesomeIcons.earthAsia},
-    {'title': '15', 'icon': FontAwesomeIcons.earthAsia},
-    {'title': '16', 'icon': FontAwesomeIcons.earthAsia},
-    {'title': '17', 'icon': FontAwesomeIcons.earthAsia},
   ];
 }
